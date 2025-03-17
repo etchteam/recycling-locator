190d2071dba875240fca4c27c4a683e401cc2868
Josh Rose <1677846+JoshTheWanderer@users.noreply.github.com>
2025-03-17 18:02:45 +0100

    ci: actions does not have a tty

--- scripts/get-next-version.sh
+++ scripts/get-next-version.sh
@@ -2,11 +2,8 @@
 
 set -euxo pipefail
 
-# Github actions don't expose /dev/tty, but tty should return a usable device
-tty=$(tty)
-
 # Run the release in dry run so that we can grab the version that this action will publish
-npm run release -- --ci false --dryRun | tee "$tty" | grep -i "Published release" > .semver-output
+npm run release -- --ci false --dryRun | grep -i "Published release" > .semver-output
 
 # Push this version to the output
 echo "current-version=$([[ $(cat .semver-output) =~ .*([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+) ]] && echo "${BASH_REMATCH[1]}")" >> "$GITHUB_OUTPUT"

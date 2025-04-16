fecd464ee44c9fcdeda44aefe533a58c6ee34c7f
Josh Rose <1677846+JoshTheWanderer@users.noreply.github.com>
2025-04-16 15:41:52 +0200

    fix: remove helper function so that the build correctly substitutes variables

--- src/config.ts
+++ src/config.ts
@@ -1,27 +1,27 @@
-/*
- * import.meta.env can be unreliable in some environments, such as tests.
- * This function falls back to process.env for those cases.
- */
-function envVar(varName: string, defaultValue?: string) {
-  return import.meta?.env?.[varName] ?? process?.env?.[varName] ?? defaultValue;
-}
-
-const LOCATOR_API = envVar(
-  'VITE_RECYCLING_LOCATOR_API',
-  'https://rl.recyclenow.com/widget/',
-);
+const LOCATOR_API =
+  import.meta?.env?.VITE_RECYCLING_LOCATOR_API ??
+  process.env.VITE_RECYCLING_LOCATOR_API ??
+  'https://rl.recyclenow.com/widget/';
 const hostname =
   typeof window !== 'undefined' ? window?.location?.hostname : 'localhost'; // Can be undefined in tests
 
 const config = {
   hostname,
-  packageVersion: envVar('VITE_PACKAGE_VERSION', '1.0.0'),
-  publicPath: envVar('VITE_PUBLIC_PATH', '/'),
-  mapsPlacesKey: envVar('VITE_HERE_MAPS_PLACES_KEY'),
-  enableAnalytics: envVar('VITE_ENABLE_ANALYTICS') === 'true',
+  packageVersion:
+    import.meta?.env?.VITE_PACKAGE_VERSION ??
+    process.env.VITE_PACKAGE_VERSION ??
+    '1.0.0',
+  publicPath:
+    import.meta?.env?.VITE_PUBLIC_PATH ?? process.env.VITE_PUBLIC_PATH ?? '/',
+  mapsPlacesKey:
+    import.meta?.env?.VITE_HERE_MAPS_PLACES_KEY ??
+    process.env.VITE_HERE_MAPS_PLACES_KEY,
+  enableAnalytics:
+    (import.meta?.env?.VITE_ENABLE_ANALYTICS ??
+      process.env.VITE_ENABLE_ANALYTICS) === 'true',
   locatorApiPath: `${LOCATOR_API}${encodeURIComponent(hostname)}/`,
   locatorAnalyticsPath: `${LOCATOR_API}analytics/record`,
-  testMode: envVar('VITE_TEST') === 'true',
+  testMode: (import.meta?.env?.VITE_TEST ?? process.env.VITE_TEST) === 'true',
 };
 
 export default config;

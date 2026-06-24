c9f9a55f48d49713d817aad2bfd35049a5a9f00f
Ella Wren <ella@etch.co>
2026-06-24 16:28:54 +0100

    feat: include bulky waste collections in nav conditional logic

--- src/components/control/CollectionsNav/CollectionsNav.tsx
+++ src/components/control/CollectionsNav/CollectionsNav.tsx
@@ -26,7 +26,7 @@ export default function CollectionsNav({
 
   return (
     <locator-context-header>
-      {propertyTypes.length > 1 ? (
+      {propertyTypes.length + Number(hasBulkyWaste) > 1 ? (
         <locator-details menu>
           <details ref={menuRef}>
             {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
